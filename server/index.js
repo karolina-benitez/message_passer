4ca1ef50b92ec381c917d96ac9f8a2dc84bfd497
Karolina Benitez <kbeniitez@gmail.com>
2020-07-16 08:47:43 -0700

    Testing qs library

--- server/index.js
+++ server/index.js
@@ -1,6 +1,25 @@
 import express from 'express';
 import cors from 'cors';
-import { getAll, getMessage, createMessage, editMessage, deleteMessage } from './controllers/messagesController.js'
+import { getAll, getMessage, createMessage, editMessage, deleteMessage } from './controllers/messagesController.js';
+// ------------testing qs-------------
+import qs from 'qs';
+import assert from 'assert';
+
+let testing = {
+  messageBody:"This is my test string"
+}
+
+var obj = qs.parse('a%5Bb%5D=c');
+assert.deepEqual(obj, { a: 'c' });
+console.log()
+var str = qs.stringify(obj);
+assert.equal(str, 'a=c');
+
+console.log(qs.stringify(testing))
+// ------------testing qs-------------
+
+// Should we use gravatar https://www.npmjs.com/package/gravatar to encrypt the message, store the encripted message in db.
+// turn encrypted messageBody into a query string
 
 const app = express();
 
