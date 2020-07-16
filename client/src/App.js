917eec74b98c02e5361e66f9f97cc3621c8645a7
naomiquinones <naomiquinones@gmail.com>
2020-07-16 11:40:11 -0700

    Change class-based components to functional with hooks

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Header from './Components/Header';
 import MessageForm from './Components/MessageForm';
+// import URLDisplay from './Components/URLDisplay';
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <MessageForm/>
+      {/* <URLDisplay /> */}
     </div>
   );
 }
--- client/src/Components/MessageForm.js
+++ client/src/Components/MessageForm.js
@@ -1,38 +1,31 @@
 import React from 'react';
 
-class MessageForm extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        value: 'Please enter your secret message.'
-      };
+const MessageForm = () => {
+  const [value, setValue] = React.useState('Please enter your secret message.')
 
-      this.handleChange = this.handleChange.bind(this);
-      this.handleSubmit = this.handleSubmit.bind(this);
+  const handleChange = event => {
+      setValue(event.target.value)
     }
 
-    handleChange(event) {
-      this.setState({value: event.target.value});
-    }
-
-    handleSubmit(event) {
-      alert('Your message was submitted: ' + this.state.value);
+  const handleSubmit = event => {
+      // alert('Your message was submitted: ' + value);
+      // alert('')
       event.preventDefault();
     }
 
-    render() {
+    // render() {
       return (
         <div>
           <h3>Enter your message</h3>
-          <form onSubmit={this.handleSubmit}>
+          <form onSubmit={handleSubmit}>
             <label>
-              <textarea value={this.state.value} onChange={this.handleChange} />
+              <textarea value={value} onChange={handleChange} />
             </label>
             <input type="submit" value="Generate URL" />
           </form>
         </div>
       );
-    }
+    // }
   }
 
 export default MessageForm;
