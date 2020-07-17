0af1ae7939af8bb3a70b4ed3440dcfab47a3c48c
naomiquinones <naomiquinones@gmail.com>
2020-07-16 22:13:29 -0700

    CHange sendSMS

--- server/messaging/sendSMS.js
+++ server/messaging/sendSMS.js
@@ -4,6 +4,7 @@ import twilio from 'twilio'
 const accountSid = process.env.TWILIO_ACCOUNT;
 const authToken = process.env.TWILIO_AUTH;
 const phone = process.env.TWILIO_PHONE;
+const recipient = process.env.RECIPIENT;
 
 const client = twilio(accountSid, authToken);
 
@@ -12,6 +13,6 @@ client.messages
   .create({
      body: 'Here is another message: ',
      from: phone,
-     to: '+13102547608'
+     to: recipient
    })
   .then(message => console.log(`If a message sent, the id is: ${message.sid}`));
